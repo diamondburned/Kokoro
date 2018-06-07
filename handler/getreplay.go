9b3db98fa55f7d92c3e4cf99c9392e624c71f5a0
Mempler <me@mempler.de>
2018-06-07 10:54:01 +0200

    Get replay from Server.

---
+++ handler/getreplay.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Gigamons/common/helpers"
+
+	"github.com/Gigamons/common/tools/usertools"
+)
+
+func GETreplaycompressed(w http.ResponseWriter, r *http.Request) {
+	validlogin := usertools.GetUser(usertools.GetUserID(r.URL.Query().Get("u"))).CheckPassword(r.URL.Query().Get("h"))
+	if !validlogin {
+		fmt.Fprint(w, "error: pass")
+		return
+	}
+	ScoreID := r.URL.Query().Get("c")
+
+	Query := "SELECT (SELECT Replay FROM replays WHERE scores.ReplayMD5 = ReplayMD5) FROM scores WHERE ScoreID = ?"
+	var Replay []byte
+	Row := helpers.DB.QueryRow(Query, ScoreID)
+	err := Row.Scan(&Replay)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(Replay)
+}
